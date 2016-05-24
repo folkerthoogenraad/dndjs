bd292e8b0f2e13871df612557ae351f3c1fd63da
JustF <408992@student.saxion.nl>
2016-05-24 18:06:08 +0200

    Added some loot stuff basics

--- server/dungeon.js
+++ server/dungeon.js
@@ -1,14 +1,22 @@
 /**
  * Creates and maintains dungeons and loot (mainly loot actually).
 */
+
+var currentLoot = [];
+
 module.exports = function(app, db, keys){
+
   app.get('/dungeon/events', function(req, res){
-    res.end(JSON.stringify([{
-      type : 'loot',
-      time : 60000,
-      items : [{
-        
-      }]
-    }]));
+    //TODO checks
+    var events = [];
+    if(currentLoot.length > 0){
+      events.push("loot");
+    }
+    //add other event stuff
+  });
+
+  app.get('dungeon/loot', function(req, res){
+    //TODO checks
+    res.end(currentLoot);
   });
 };
