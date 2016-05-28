e7f0c5ed733e8bf43e63d60c1cb827e7a88c77bb
JustF <408992@student.saxion.nl>
2016-05-28 15:57:37 +0200

    Added the admin controllers, this time for real

---
+++ js/adminController.js
@@ -0,0 +1,18 @@
+app.controller('AdminController', ['$scope', '$location', '$http', '$uibModal', function($scope, $location, $http, $uibModal) {
+  $scope.items = function(){
+
+  };
+
+  $scope.gold = function(){
+
+  };
+
+  $scope.loot = function(){
+
+  };
+
+  $scope.exit = function(){
+    $location.path("home");
+  };
+
+}]);
